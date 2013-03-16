3f7825120adcede6092551c4b1fd1b883349baa8
Mikkel Krautz <mikkel@krautz.dk>
2013-03-16 14:58:10 +0100

    MUServerButton: add a big server button to be used in iPad server lists.

--- Source/Classes/MUServerButton.h
+++ Source/Classes/MUServerButton.h
@@ -1,13 +1,10 @@
-// Copyright 2009-2011 The 'Mumble for iOS' Developers. All rights reserved.
+// Copyright 2013 The 'Mumble for iOS' Developers. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-#import <MumbleKit/MKServerPinger.h>
+#import "MUFavouriteServer.h"
 
-@class MUFavouriteServer;
-
-@interface MUServerCell : UITableViewCell <MKServerPingerDelegate>
-+ (NSString *) reuseIdentifier;
+@interface MUServerButton : UIControl
 - (void) populateFromDisplayName:(NSString *)displayName hostName:(NSString *)hostName port:(NSString *)port;
 - (void) populateFromFavouriteServer:(MUFavouriteServer *)favServ;
 @end
