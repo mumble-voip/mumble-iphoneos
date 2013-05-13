3f87e0cc797b5d0068359788e37e0803a2495270
Mikkel Krautz <mikkel@krautz.dk>
2013-05-13 23:05:12 +0200

    MUApplicationDelegate: implement MKAudioDelegate to control MKAudio's state change decisions from Mumble.
    Fixes #68.

--- Source/Classes/MUApplicationDelegate.m
+++ Source/Classes/MUApplicationDelegate.m
@@ -18,7 +18,9 @@
 
 #import "PLCrashReporter.h"
 
-@interface MUApplicationDelegate () <UIApplicationDelegate, UIAlertViewDelegate> {
+@interface MUApplicationDelegate () <UIApplicationDelegate,
+                                     UIAlertViewDelegate,
+                                     MKAudioDelegate> {
     UIWindow                  *_window;
     UINavigationController    *_navigationController;
     MUPublicServerListFetcher *_publistFetcher;
@@ -330,6 +332,23 @@
 
 - (void) connectionClosed:(NSNotification *)notification {
     _connectionActive = NO;
+}
+
+// Implement MKAudioDelegate.
+- (BOOL) audioShouldBeRunning:(MKAudio *)audio {
+    UIApplication *app = [[UIApplication sharedApplication] applicationState];
+    UIApplicationState state = [app applicationState];
+    switch (state) {
+        case UIApplicationStateActive:
+            // When in the foreground, we always keep MKAudio running.
+            return YES;
+        case UIApplicationStateBackground:
+        case UIApplicationStateInactive:
+            // When backgrounded, only turn on MKAudio if we're connected
+            // to a server.
+            return _connectionActive;
+    }
+    return NO;
 }
 
 - (void) applicationWillResignActive:(UIApplication *)application {
