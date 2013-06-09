1de57fe790a2cacf8fce70c3d83a9422adbc6c71
Mikkel Krautz <mikkel@krautz.dk>
2013-06-09 15:30:08 +0200

    MUTextMessage: use passed-in date instead of [NSDate date] in factory method.

--- Source/Classes/MUTextMessage.m
+++ Source/Classes/MUTextMessage.m
@@ -63,7 +63,7 @@
 }
 
 + (MUTextMessage *) textMessageWithHeading:(NSString *)heading andMessage:(NSString *)msg andEmbeddedLinks:(NSArray *)links andEmbeddedImages:(NSArray *)images andTimestampDate:(NSDate *)timestampDate isSentBySelf:(BOOL)sentBySelf {
-    return [[[MUTextMessage alloc] initWithHeading:heading andMessage:msg andDate:[NSDate date] andEmbeddedLinks:links andEmbeddedImages:images  andTimestampDate:timestampDate andSentBySelf:sentBySelf] autorelease];
+    return [[[MUTextMessage alloc] initWithHeading:heading andMessage:msg andDate:timestampDate andEmbeddedLinks:links andEmbeddedImages:images  andTimestampDate:timestampDate andSentBySelf:sentBySelf] autorelease];
 }
 
 @end
