bc583433b9820eb2466a9701f3cc3e5d65078a91
fridtjof <fridtjof@das-labor.org>
2025-04-29 21:58:22 +0200

    pch: move MobileCoreServices import into its only user
    MobileCoreServices was also renamed to just CoreServices at some point (supposedly XCode 11?)

--- Source/Classes/MUCertificateViewController.m
+++ Source/Classes/MUCertificateViewController.m
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+#import <CoreServices/CoreServices.h>
+
 #import "MUCertificateViewController.h"
 #import "MUTableViewHeaderLabel.h"
 #import "MUCertificateController.h"
--- Source/Classes/MUMessagesViewController.m
+++ Source/Classes/MUMessagesViewController.m
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+#import <CoreServices/CoreServices.h>
 #import <UserNotifications/UserNotifications.h>
 
 #import <MumbleKit/MKServerModel.h>
