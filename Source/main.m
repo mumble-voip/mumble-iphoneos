55178c9138fca9683b243775046078b9d44916da
fridtjof <fridtjof@das-labor.org>
2025-05-01 13:41:34 +0200

    main: get class name from actual delegate class
    This is taken from current (XCode 16) "new project" templates. It minimizes the autorelease pool's scope as well

--- Source/main.m
+++ Source/main.m
@@ -2,9 +2,15 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+@import UIKit;
+
+#import "Classes/MUApplicationDelegate.h"
+
 int main(int argc, char *argv[]) {
+    NSString * appDelegateClassName;
     @autoreleasepool {
-        int retVal = UIApplicationMain(argc, argv, @"UIApplication", @"MUApplicationDelegate");
-        return retVal;
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([MUApplicationDelegate class]);
     }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
 }
