bd33265a7a2eb95f82ee100b8834176fbeae4778
fridtjof <fridtjof@das-labor.org>
2025-06-12 15:32:48 +0200

    remove MUVersionChecker

--- Source/Classes/MUVersionChecker.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2009-2010 The 'Mumble for iOS' Developers. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-@import Foundation;
-
-@interface MUVersionChecker : NSObject
-- (id) init;
-@end
